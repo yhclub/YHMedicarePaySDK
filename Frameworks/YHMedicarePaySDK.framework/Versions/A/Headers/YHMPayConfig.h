37a89718e5be073c355fb36a6680b66b8337a284
oid <42318168@qq.com>
2018-10-11 15:07:53 +0800

    fix:1.0.2.7

--- Frameworks/YHMedicarePaySDK.framework/Versions/A/Headers/YHMPayConfig.h
+++ Frameworks/YHMedicarePaySDK.framework/Versions/A/Headers/YHMPayConfig.h
@@ -7,23 +7,6 @@
 
 #import <YHOnePaySDK/YHOnePayConfig.h>
 
-//商户所属渠道类型，请修改为对应的渠道
-/*
- 01 窗口
- 02 自助机
- 03 网站
- 04 手机app
- 05 微信公众号
- 06 银医通
- 07 微信小程序
- 08 支付宝服务窗
- 09 药店
- 14 市民卡app
- 15 i厦门app
- 31 美丽厦门微信公众号
- */
-#define YHMPAppChannelType @"15"
-
 
 @interface YHMPayConfig : YHOnePayConfig
 
@@ -37,15 +20,33 @@
  */
 @property(nonatomic,copy)NSString * appSecret;
 
+/**
+ * 商户所属渠道类型，请修改为对应的渠道
+ *  01 窗口
+ *  02 自助机
+ *  03 网站
+ *  04 手机app
+ *  05 微信公众号
+ *  06 银医通
+ *  07 微信小程序
+ *  08 支付宝服务窗
+ *  09 药店
+ *  14 市民卡app
+ *  15 i厦门app
+ *  31 美丽厦门微信公众号
+ */
+@property(nonatomic,copy)NSString * appChannelType;
+
 
 /**
  初始化设置
  
- @param appId appId（注应用ID与应用的bundleIdentifier对应）
- @param appSecret appSecret
+ @param appId 应用ID与应用的bundleIdentifier对应
+ @param appSecret 应用密钥
+ @param appChannelType 所属渠道类型
  @param platformHost 平台地址
  */
-+(void)initWithAPPID:(NSString *)appId appSecret:(NSString *)appSecret platformHost:(NSString *)platformHost;
++(YHMPayConfig *)initWithAPPID:(NSString *)appId appSecret:(NSString *)appSecret channelType:(NSString *)appChannelType platformHost:(NSString *)platformHost;
 
 
 @end
