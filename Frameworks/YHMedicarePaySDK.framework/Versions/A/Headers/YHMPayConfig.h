0b259894979198b98fe47c36ab6b3d70c7018fce
oid <42318168@qq.com>
2018-10-11 13:35:46 +0800

    fix:修复bug

--- Frameworks/YHMedicarePaySDK.framework/Versions/A/Headers/YHMPayConfig.h
+++ Frameworks/YHMedicarePaySDK.framework/Versions/A/Headers/YHMPayConfig.h
@@ -7,6 +7,24 @@
 
 #import <YHOnePaySDK/YHOnePayConfig.h>
 
+//商户所属渠道类型，请修改为对应的渠道
+/*
+ 01 窗口
+ 02 自助机
+ 03 网站
+ 04 手机app
+ 05 微信公众号
+ 06 银医通
+ 07 微信小程序
+ 08 支付宝服务窗
+ 09 药店
+ 14 市民卡app
+ 15 i厦门app
+ 31 美丽厦门微信公众号
+ */
+#define YHMPAppChannelType @"15"
+
+
 @interface YHMPayConfig : YHOnePayConfig
 
 /**
