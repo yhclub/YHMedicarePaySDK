82fed9aeb90e5e81931c3d2b76d05fa5c6b77ea2
liaojiangtu <42318168@qq.com>
2020-07-09 15:24:52 +0800

    feat:修复签名失败bug

--- Frameworks/YHMedicarePaySDK.framework/Versions/A/Headers/YHMedicarePaySDK.h
+++ Frameworks/YHMedicarePaySDK.framework/Versions/A/Headers/YHMedicarePaySDK.h
@@ -6,6 +6,7 @@
 //  Copyright © 2018年 yh. All rights reserved.
 //
 //  Version 1.0.9.2  by Jagtu  2018/11/21
+//  Version 1.1.9.1  by Jagtu  2020/07/09
 //
 
 // In this header, you should import all the public headers of your framework using statements like #import <YHMedicarePaySDK/PublicHeader.h>
